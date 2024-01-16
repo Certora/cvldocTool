6059854bddafae980a4ae1ea770c8b4c93138459
liav-certora <liav@certora.com>
2024-01-16 15:49:22 +0200

    first try

--- tests/customer_code_test.py
+++ tests/customer_code_test.py
@@ -1,26 +1,26 @@
-from . import util
+from . import test_cvldoc
 import os
 
 
-def test_erc1155_burnable():
-    util.check_single_file("customer_code/ERC1155Burnable.spec")
+# def test_erc1155_burnable():
+#     util.check_single_file("customer_code/ERC1155Burnable.spec")
 
 
-def test_erc1155_new():
-    util.check_single_file("customer_code/ERC1155New.spec")
+# def test_erc1155_new():
+#     util.check_single_file("customer_code/ERC1155New.spec")
 
 
-def test_erc1155_pausable():
-    util.check_single_file("customer_code/ERC1155Pausable.spec")
+# def test_erc1155_pausable():
+#     util.check_single_file("customer_code/ERC1155Pausable.spec")
 
 
-def test_erc1155_supply():
-    util.check_single_file("customer_code/ERC1155Supply.spec")
+# def test_erc1155_supply():
+#     util.check_single_file("customer_code/ERC1155Supply.spec")
 
 
-def test_governor_prevent_late_quorum():
-    util.check_single_file("customer_code/GovernorPreventLateQuorum.spec")
+# def test_governor_prevent_late_quorum():
+#     util.check_single_file("customer_code/GovernorPreventLateQuorum.spec")
 
 
-def test_initializable():
-    util.check_single_file("customer_code/Initializable.spec")
+# def test_initializable():
+#     util.check_single_file("customer_code/Initializable.spec")
---
+++ tests/test_cvldoc.py
@@ -0,0 +1,37 @@
+from src.CVLDoc import cvldoc_to_json
+
+import json
+from pathlib import Path
+
+from deepdiff import DeepDiff
+import pytest
+
+
+def __file_contents_as_json(path: Path) -> dict:
+    with open(path, 'r+') as f:
+        content = f.read()
+
+    content.replace('\r\n', '\n')  # normalize line endings
+    return json.loads(content)
+
+
+def __get_diff(absolute_spec_path: Path) -> DeepDiff:
+    expected_path = Path(str(absolute_spec_path).replace('.spec', '-expected.json'))
+    expected_json = __file_contents_as_json(expected_path)
+
+    output_path = Path(str(absolute_spec_path).replace('.spec', '-cvldoc.json'))
+    output_json = __file_contents_as_json(output_path)
+    output_path.unlink()
+
+    return DeepDiff(expected_json, output_json, ignore_order=False)
+
+
+@pytest.mark.parametrize('spec_path', Path(__file__).parent.rglob('*.spec'))
+def test_cvldoc_generation(spec_path: Path):
+    absolute_path = spec_path.absolute()
+
+    args = ['-u', str(absolute_path)]
+    cvldoc_to_json.main(args)
+
+    if diff := __get_diff(absolute_path):
+        assert 0, diff.pretty()
