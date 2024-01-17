ba09b37664480a009f0a96a169267c25b9d8d608
liav-certora <liav@certora.com>
2024-01-17 20:02:25 +0200

    blacked the code

--- tests/test_cvldoc.py
+++ tests/test_cvldoc.py
@@ -7,35 +7,36 @@ from deepdiff import DeepDiff
 import pytest
 
 
-SPECS = list(Path(__file__).parent.rglob('*.spec'))
+SPECS = list(Path(__file__).parent.rglob("*.spec"))
 
 
-def __file_contents_as_json(path: Path) -> dict:
-    with open(path, 'r+') as f:
+def file_contents_as_json(path: Path) -> dict:
+    with open(path, "r+") as f:
         content = f.read()
 
-    content.replace('\r\n', '\n')  # normalize line endings
+    content.replace("\r\n", "\n")  # normalize line endings
     return json.loads(content)
 
 
-def __get_diff(absolute_spec_path: Path) -> DeepDiff:
-    expected_path = Path(str(absolute_spec_path).replace('.spec', '-expected.json'))
-    expected_json = __file_contents_as_json(expected_path)
+def get_diff(absolute_spec_path: Path) -> DeepDiff:
+    expected_path = Path(str(absolute_spec_path).replace(".spec", "-expected.json"))
+    expected_json = file_contents_as_json(expected_path)
 
-    output_path = Path(str(absolute_spec_path).replace('.spec', '-cvldoc.json'))
-    output_json = __file_contents_as_json(output_path)
+    output_path = Path(str(absolute_spec_path).replace(".spec", "-cvldoc.json"))
+    output_json = file_contents_as_json(output_path)
     output_path.unlink()
 
     return DeepDiff(expected_json, output_json, ignore_order=False)
 
 
-
-@pytest.mark.parametrize('spec_path', SPECS, ids=[str(s.relative_to(Path(__file__).parent)) for s in SPECS])
+@pytest.mark.parametrize(
+    "spec_path", SPECS, ids=[str(s.relative_to(Path(__file__).parent)) for s in SPECS]
+)
 def test_cvldoc_generation(spec_path: Path):
     absolute_spec_path = spec_path.absolute()
 
-    args = ['-u', str(absolute_spec_path)]
+    args = ["-u", str(absolute_spec_path)]
     cvldoc_to_json.main(args)
 
-    if diff := __get_diff(absolute_spec_path):
+    if diff := get_diff(absolute_spec_path):
         assert 0, diff.pretty()
